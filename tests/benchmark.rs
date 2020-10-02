96ad5b565680121f9f39894279441f5d9253a39f
Mingcong Han <hanmingcong123@hotmail.com>
2020-10-02 17:56:31 +0800

    Add lockfree cuckoohash with basic methods(search, insert and remove) (#1)

---
+++ tests/benchmark.rs
@@ -0,0 +1,210 @@
+use lockfree_cuckoohash::{pin, LockFreeCuckooHash};
+use num_cpus;
+use rand::Rng;
+use std::sync::Arc;
+use std::time::Instant;
+
+fn bench(
+    init_capacity: usize,
+    capacity: usize,
+    warmup_factor: f32,
+    write_factor: f32,
+    remove_factor: f32,
+    num_read_per_write: usize,
+    num_thread: usize,
+) -> (usize, f64) {
+    assert!(warmup_factor < 1.0);
+    assert!(write_factor + warmup_factor < 1.0);
+    assert!(remove_factor < write_factor);
+
+    let warmup_size = (capacity as f32 * warmup_factor) as usize;
+
+    let cuckoo_map = Arc::new(LockFreeCuckooHash::with_capacity(init_capacity));
+
+    let guard = pin();
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..warmup_size {
+        let key: u32 = rng.gen();
+        let value: u32 = rng.gen();
+
+        cuckoo_map.insert_with_guard(key, value, &guard);
+    }
+
+    let insert_per_thread = (capacity as f32 * write_factor) as usize / num_thread;
+    let remove_per_thread = (capacity as f32 * remove_factor) as usize / num_thread;
+
+    let mut insert_entries = Vec::with_capacity(insert_per_thread * num_thread);
+    for _ in 0..(insert_per_thread * num_thread) {
+        let key: u32 = rng.gen();
+        let value: u32 = rng.gen();
+        insert_entries.push((key, value));
+    }
+    let insert_entries = Arc::new(insert_entries);
+    let mut handles = Vec::with_capacity(num_thread);
+    let start = Instant::now();
+    for i in 0..num_thread {
+        let map = cuckoo_map.clone();
+        let insert_entries = insert_entries.clone();
+        let handle = std::thread::spawn(move || {
+            let mut remove_flag: f32 = 0.0;
+            let mut remove_idx = 0;
+            let num_remove_per_thread = remove_factor / write_factor;
+            let mut rng = rand::thread_rng();
+            let guard = &pin();
+            let insert_entries =
+                &insert_entries[i * insert_per_thread..(i + 1) * insert_per_thread];
+            for i in 0..insert_per_thread {
+                // 1. insert a kv pair
+                map.insert_with_guard(insert_entries[i].0, insert_entries[i].1, guard);
+
+                // 2. read num_read_per_write kv pairs
+                for _ in 0..num_read_per_write {
+                    let key_idx: usize = rng.gen::<usize>() % (i + 1);
+                    map.search_with_guard(&insert_entries[key_idx].0, guard);
+                }
+
+                // 3. remove num_remove_per_write kv pairs
+                remove_flag += num_remove_per_thread;
+                if remove_flag >= 1.0 {
+                    map.remove_with_guard(&insert_entries[remove_idx].0, guard);
+                    remove_flag -= 1.0;
+                    remove_idx += 1;
+                }
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let duration = start.elapsed().as_millis() as f64;
+    let ops = (insert_per_thread + remove_per_thread + insert_per_thread * num_read_per_write)
+        * num_thread;
+    guard.flush();
+    (ops, duration)
+}
+
+fn bench_read_write(num_thread: usize) {
+    let capacity = 10_000_000;
+    let warmup_factor = 0.01;
+    let insert_factor = 0.2;
+    let remove_factor = 0.0;
+    let num_read_per_write = 19;
+    let (ops, duration) = bench(
+        capacity,
+        capacity,
+        warmup_factor,
+        insert_factor,
+        remove_factor,
+        num_read_per_write,
+        num_thread,
+    );
+
+    let num_remove_per_write = remove_factor / insert_factor;
+
+    println!(
+        "num_thread: {}, init_capacity: {}, capacity: {}, ops: {}, search: {}%, insert: {}%, remove: {}%, duration: {}ms, throughput: {}op/ms",
+        num_thread, capacity, capacity, ops,
+        num_read_per_write as f32 / (num_read_per_write as f32 + num_remove_per_write + 1.0) * 100.0,
+        1.0 / (num_read_per_write as f32 + num_remove_per_write + 1.0)* 100.0,
+        num_remove_per_write as f32 / (num_read_per_write as f32 + num_remove_per_write + 1.0) * 100.0,
+        duration, (ops as f64 / duration)
+    );
+}
+
+#[test]
+#[ignore]
+fn bench_read_write_scale() {
+    let cpu_cores = num_cpus::get();
+    println!("Bench: search + insert, num_cpu_cores: {}", cpu_cores);
+    let mut num_thread = 1;
+    loop {
+        if num_thread > cpu_cores {
+            break;
+        }
+        bench_read_write(num_thread);
+        num_thread *= 2;
+    }
+}
+
+fn bench_insert_only(num_thread: usize) {
+    let capacity = 10_000_000;
+    let warmup_factor = 0.0;
+    let insert_factor = 0.4;
+    let remove_factor = 0.0;
+    let num_read_per_write = 0;
+    let (ops, duration) = bench(
+        capacity,
+        capacity,
+        warmup_factor,
+        insert_factor,
+        remove_factor,
+        num_read_per_write,
+        num_thread,
+    );
+    let guard = pin();
+    guard.flush();
+    println!(
+        "num_thread: {}, init_capacity: {}, capacity: {}, insert: {}, duration: {}ms, throughput: {}op/ms",
+        num_thread, capacity, capacity, insert_factor * capacity as f32,
+        duration, (ops as f64 / duration)
+    );
+}
+
+#[test]
+#[ignore]
+fn bench_insert_scale() {
+    let cpu_cores = num_cpus::get();
+    println!("Bench: insert only, num_cpu_cores: {}", cpu_cores);
+    let mut num_thread = 1;
+    loop {
+        if num_thread > cpu_cores {
+            break;
+        }
+        bench_insert_only(num_thread);
+        num_thread *= 2;
+    }
+}
+
+fn bench_insert_resize(num_thread: usize) {
+    let init_capacity = 5_000_000;
+    let capacity = 10_000_000;
+    let warmup_factor = 0.0;
+    let insert_factor = 0.5;
+    let remove_factor = 0.0;
+    let num_read_per_write = 0;
+    let (ops, duration) = bench(
+        init_capacity,
+        capacity,
+        warmup_factor,
+        insert_factor,
+        remove_factor,
+        num_read_per_write,
+        num_thread,
+    );
+    let guard = pin();
+    guard.flush();
+    println!(
+        "num_thread: {}, init_capacity: {}, capacity: {}, insert: {}, duration: {}ms, throughput: {}op/ms",
+        num_thread, init_capacity, capacity, insert_factor * capacity as f32,
+        duration, (ops as f64 / duration)
+    );
+}
+
+#[test]
+#[ignore]
+fn bench_resize_scale() {
+    let cpu_cores = num_cpus::get();
+    println!("Bench: insert with resize, num_cpu_cores: {}", cpu_cores);
+    let mut num_thread = 1;
+    loop {
+        if num_thread > cpu_cores {
+            break;
+        }
+        bench_insert_resize(num_thread);
+        num_thread *= 2;
+    }
+}
---
+++ tests/integration_test.rs
@@ -0,0 +1,201 @@
+use lockfree_cuckoohash::{pin, LockFreeCuckooHash};
+use rand::Rng;
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+
+#[test]
+fn test_single_thread() {
+    let capacity: usize = 100_000;
+    let load_factor: f32 = 0.3;
+    let remove_factor: f32 = 0.1;
+    let size = (capacity as f32 * load_factor) as usize;
+
+    let mut base_map: HashMap<u32, u32> = HashMap::with_capacity(capacity);
+    let cuckoo_map: LockFreeCuckooHash<u32, u32> = LockFreeCuckooHash::with_capacity(capacity);
+
+    let mut rng = rand::thread_rng();
+    let guard = pin();
+
+    for _ in 0..size {
+        let key: u32 = rng.gen();
+        let value: u32 = rng.gen();
+
+        base_map.insert(key, value);
+        cuckoo_map.insert_with_guard(key, value, &guard);
+
+        let r: u8 = rng.gen();
+        let need_remove = (r % 10) < ((remove_factor * 10_f32) as u8);
+        if need_remove {
+            base_map.remove(&key);
+            cuckoo_map.remove_with_guard(&key, &guard);
+        }
+    }
+
+    assert_eq!(base_map.len(), cuckoo_map.size());
+
+    for (key, value) in base_map {
+        let value2 = cuckoo_map.search_with_guard(&key, &guard);
+        assert_eq!(value, *value2.unwrap());
+    }
+}
+
+#[test]
+fn test_single_thread_resize() {
+    let init_capacity: usize = 32;
+    let size = 1024;
+
+    let mut base_map: HashMap<u32, u32> = HashMap::with_capacity(init_capacity);
+    let cuckoo_map: LockFreeCuckooHash<u32, u32> = LockFreeCuckooHash::with_capacity(init_capacity);
+
+    let mut rng = rand::thread_rng();
+    let guard = pin();
+
+    for _ in 0..size {
+        let mut key: u32 = rng.gen();
+        while base_map.contains_key(&key) {
+            key = rng.gen();
+        }
+        let value: u32 = rng.gen();
+
+        base_map.insert(key, value);
+        cuckoo_map.insert_with_guard(key, value, &guard);
+    }
+
+    assert_eq!(base_map.len(), cuckoo_map.size());
+    assert_eq!(cuckoo_map.size(), size);
+    for (key, value) in base_map {
+        let value2 = cuckoo_map.search_with_guard(&key, &guard);
+        assert_eq!(value, *value2.unwrap());
+    }
+}
+
+#[test]
+fn test_multi_thread() {
+    let capacity: usize = 1_000_000;
+    let load_factor: f32 = 0.2;
+    let num_thread: usize = 4;
+
+    let size = (capacity as f32 * load_factor) as usize;
+    let warmup_size = size / 3;
+
+    let mut warmup_entries: Vec<(u32, u32)> = Vec::with_capacity(warmup_size);
+
+    let mut new_insert_entries: Vec<(u32, u32)> = Vec::with_capacity(size - warmup_size);
+
+    let mut base_map: HashMap<u32, u32> = HashMap::with_capacity(capacity);
+    let cuckoo_map: LockFreeCuckooHash<u32, u32> = LockFreeCuckooHash::with_capacity(capacity);
+
+    let mut rng = rand::thread_rng();
+    let guard = pin();
+
+    for _ in 0..warmup_size {
+        let mut key: u32 = rng.gen();
+        while base_map.contains_key(&key) {
+            key = rng.gen();
+        }
+        let value: u32 = rng.gen();
+        base_map.insert(key, value);
+        cuckoo_map.insert_with_guard(key, value, &guard);
+        warmup_entries.push((key, value));
+    }
+
+    for _ in 0..(size - warmup_size) {
+        let mut key: u32 = rng.gen();
+        while base_map.contains_key(&key) {
+            key = rng.gen();
+        }
+        let value: u32 = rng.gen();
+        new_insert_entries.push((key, value));
+        base_map.insert(key, value);
+    }
+
+    let mut handles = Vec::with_capacity(num_thread);
+    let insert_count = Arc::new(AtomicUsize::new(0));
+    let cuckoo_map = Arc::new(cuckoo_map);
+    let warmup_entries = Arc::new(warmup_entries);
+    let new_insert_entries = Arc::new(new_insert_entries);
+    for _ in 0..num_thread {
+        let insert_count = insert_count.clone();
+        let cuckoo_map = cuckoo_map.clone();
+        let warmup_entries = warmup_entries.clone();
+        let new_insert_entries = new_insert_entries.clone();
+        let handle = std::thread::spawn(move || {
+            let guard = pin();
+            let mut entry_idx = insert_count.fetch_add(1, Ordering::SeqCst);
+            let mut rng = rand::thread_rng();
+            while entry_idx < new_insert_entries.len() {
+                // read 5 pairs ,then insert 1 pair.
+                for _ in 0..5 {
+                    let rnd_idx: usize = rng.gen_range(0, warmup_entries.len());
+                    let warmup_entry = &warmup_entries[rnd_idx];
+                    let res = cuckoo_map.search_with_guard(&warmup_entry.0, &guard);
+                    assert_eq!(res.is_some(), true);
+                    assert_eq!(*res.unwrap(), warmup_entry.1);
+                }
+                let insert_pair = &new_insert_entries[entry_idx];
+                cuckoo_map.insert_with_guard(insert_pair.0, insert_pair.1, &guard);
+                entry_idx = insert_count.fetch_add(1, Ordering::SeqCst);
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    for (k, v) in base_map {
+        let v2 = cuckoo_map.search_with_guard(&k, &guard);
+        assert_eq!(v, *v2.unwrap());
+    }
+}
+
+#[test]
+fn test_multi_thread_resize() {
+    let insert_per_thread = 100000;
+    let num_thread = 8;
+    let cuckoo_map: LockFreeCuckooHash<u32, u32> = LockFreeCuckooHash::with_capacity(8);
+    let mut base_map: HashMap<u32, u32> = HashMap::new();
+    let mut entries: Vec<(u32, u32)> = Vec::with_capacity(insert_per_thread * num_thread);
+
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..(insert_per_thread * num_thread) {
+        let mut key: u32 = rng.gen();
+        while base_map.contains_key(&key) {
+            key = rng.gen();
+        }
+        let value: u32 = rng.gen();
+        base_map.insert(key, value);
+        entries.push((key, value));
+    }
+
+    let mut handles = Vec::with_capacity(num_thread);
+    let cuckoo_map = Arc::new(cuckoo_map);
+    let entries = Arc::new(entries);
+    for thread_idx in 0..num_thread {
+        let cuckoo_map = cuckoo_map.clone();
+        let entries = entries.clone();
+        let handle = std::thread::spawn(move || {
+            let guard = pin();
+            let begin = thread_idx * insert_per_thread;
+            for i in begin..(begin + insert_per_thread) {
+                cuckoo_map.insert_with_guard(entries[i].0, entries[i].1, &guard);
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let guard = pin();
+    assert_eq!(base_map.len(), cuckoo_map.size());
+    for (k, v) in base_map {
+        let v2 = cuckoo_map.search_with_guard(&k, &guard);
+        assert!(v2.is_some());
+        assert_eq!(v, *v2.unwrap());
+    }
+}
